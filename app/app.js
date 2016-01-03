7444366b481c38e88d4a5721bed8ef237f978adf
Nim <nim@nim.lt>
2016-01-03 15:23:43 +0200

    started mockup

--- app/app.js
+++ app/app.js
@@ -2,11 +2,16 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
+	'ngRoute',
+	'myApp.combatGear',
+	'myApp.version'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
+	$routeProvider.otherwise({redirectTo: '/combatGear'});
+
+	$routeProvider.when('/combatGear', {
+		templateUrl: 'modules/combatGear/combatGear.html',
+		controller: 'combatGearCtrl'
+	});
+
 }]);
---
+++ app/modules/combatGear/combatGear-ctrl.js
@@ -0,0 +1,6 @@
+angular.module('myApp.combatGear', ['ngRoute'])
+	.controller('combatGearCtrl', ['$scope', function ($scope) {
+
+		$scope.gear = [];
+
+	}]);
