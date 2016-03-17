ea43f0a86b59ed1469d727922744357427c03410
Daumantas Girininkas <daumantas.girininkas@fltechnics.com>
2016-03-17 10:56:04 +0200

    combar skills css corrected to show in more than one column

--- app/modules/combat/combat-ctrl.js
+++ app/modules/combat/combat-ctrl.js
@@ -6,7 +6,7 @@ angular.module('myApp.combat', ['ngRoute'])
 		$scope.autofail = 6;
 		$scope.luck = character.luck;
 
-		$scope.activeWeapon = character.weapons[0].name; // galvok blem kaip pasirinkt aktyvu weapona...
+		$scope.activeWeapon = character.weapons[0].name;
 
 		$scope.history = turnsSrv.history;
 
@@ -48,34 +48,5 @@ angular.module('myApp.combat', ['ngRoute'])
 		$scope.undoTurn = function() {
 			turnsSrv.history.splice(turnsSrv.history.length - 1, 1);
 		}
-
-/*
-
-		var weaponType =  character.weapons[0].type;
-
-		console.log(weaponType);
-		console.log(Object.keys(character.combatSkills));
-
-
-			for ( key in character.combatSkills) {
-				var value = character.combatSkills[key];
-				var objektoVardas = Object.keys(character.combatSkills);
-
-
-				console.log(objektoVardas[key]);
-				console.log(value);
-				if (weaponType == character.combatSkills.)
-			}
-
-
-
-		$scope.mainhandAV = character.attributes.REF + value; // reikia paimti aktyvu ginkla.
-		$scope.mainhandDV = character.attributes.REF + character.combatSkills.Blade;
-
-*/
-
-
-
-		//kaip pasirinkti nauja ginkla is turimu combat gear? ar tai popupas ar lentele ar kas turi but kad veiktu ir ant plansetes...
 
 	}]);
