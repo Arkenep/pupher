46fda7d5a3445660ccfa4cd092ba2abeae6db0d6
Nim <nim@nim.lt>
2016-04-10 15:05:46 +0300

    turns service created

--- app/modules/combat/combat-ctrl.js
+++ app/modules/combat/combat-ctrl.js
@@ -1,9 +1,23 @@
 angular.module('myApp.combat', ['ngRoute'])
 	.controller('combatCtrl', ['$scope', 'character', 'turnsSrv', function ($scope, character, turnsSrv) {
 
+		$scope.endTurn = function() {
+			turnsSrv.endTurn();
+		};
+
+		$scope.spendLuck = function(n) {
+			if(turnsSrv.spendLuck(n)) {
+				console.log(n + " luck has been spent");
+			} else {
+				console.log(n + " no luck pal");
+			}
+		};
+
+		$scope.character = character; //cia susitrumpina ir reikia pareiti per visus sh kuriuos levai apsirasiau.
+
 		//console.log(character);
 		$scope.autofail = 6;
-		$scope.luck = character.luck;
+		//$scope.luck = character.luck;
 
 		$scope.currentMainHandSkill = character.currentMainHandSkill;
 		$scope.currentOffHandSkill = character.currentOffHandSkill;
@@ -35,264 +49,17 @@ angular.module('myApp.combat', ['ngRoute'])
 			character.currentMainHandSkill = skill;
 			$scope.characterMHAV = character.combatSkills[skill] + character.attributes.REF + $scope.mainHandWeapon.attackValueBonus;
 			$scope.characterMHDV = character.combatSkills[skill] + character.attributes.REF + $scope.mainHandWeapon.defenceValueBonus;
-			character.characterMHAV = $scope.characterMHAV;
-			character.characterMHDV = $scope.characterMHDV;
+
 		};
-		
+
 		$scope.setOHActiveSkill = function(skill) {
 			$scope.currentOffHandSkill = skill;
 			character.currentOffHandSkill = skill;
 			$scope.characterOHAV = character.combatSkills[skill] + character.attributes.REF + $scope.offHandWeapon.attackValueBonus;
 			$scope.characterOHDV = character.combatSkills[skill] + character.attributes.REF + $scope.offHandWeapon.defenceValueBonus;
-			character.characterOHAV = $scope.characterOHAV;
-			character.characterOHDV = $scope.characterOHDV;
-		};
-	
-		$scope.history = turnsSrv.history;
-		
-		$scope.thisTurn = {
-			initiative : 0,
-			bonusAV : 0,
-			equipment : {
-				mainHandWeaponSkill : {
-					name: '',
-					level: 0
-				},
-				offHandWeaponSkill : {
-					name: '',
-					level: 0
-				},
-				mainHandWeaponAV : 0,
-				mainHandWeaponDV : 0,
-				offHandWeaponAV : 0,
-				offHandWeaponDV : 0
-			},
-			action : [
-				{
-					type: '',
-					attackType: '',
-					bonusAVUsed: 0,
-					location: {
-						name: '',
-						vigorCost: 0
-					},
-					specials: {
-						name: '',
-						vigorCost: 0
-					},
-					effects: {
-						name: '',
-						vigorCost: 0
-					},
-					roll: 0,
-					finalAV: 0,
-					vigor: 0,
-					luck: 0,
-					sublocation: '',
-					damageDone: 0,
-					successfulEffects: ''
-				},
-				{
-					type: '',
-					attackType: '',
-					bonusAVUsed: 0,
-					location: {
-						name: '',
-						vigorCost: 0
-					},
-					specials: {
-						name: '',
-						vigorCost: 0
-					},
-					effects: {
-						name: '',
-						vigorCost: 0
-					},
-					roll: 0,
-					finalAV: 0,
-					vigor: 0,
-					luck: 0,
-					sublocation: '',
-					damageDone: 0,
-					successfulEffects: ''
-				},
-				{
-					type: '',
-					attackType: '',
-					bonusAVUsed: 0,
-					location: {
-						name: '',
-						vigorCost: 0
-					},
-					specials: {
-						name: '',
-						vigorCost: 0
-					},
-					effects: {
-						name: '',
-						vigorCost: 0
-					},
-					roll: 0,
-					finalAV: 0,
-					vigor: 0,
-					luck: 0,
-					sublocation: '',
-					damageDone: 0,
-					successfulEffects: ''
-				},
-				{
-					type: '',
-					attackType: '',
-					bonusAVUsed: 0,
-					location: {
-						name: '',
-						vigorCost: 0
-					},
-					specials: {
-						name: '',
-						vigorCost: 0
-					},
-					effects: {
-						name: '',
-						vigorCost: 0
-					},
-					roll: 0,
-					finalAV: 0,
-					vigor: 0,
-					luck: 0,
-					sublocation: '',
-					damageDone: 0,
-					successfulEffects: ''
-				}
-			]
+
 		};
 
-		$scope.resetTurn = function() {
-			$scope.thisTurn = {
-				initiative : 0,
-				bonusAV : 0,
-				equipment : {
-					mainHandWeaponSkill : {
-						name: '',
-						level: 0
-					},
-					offHandWeaponSkill : {
-						name: '',
-						level: 0
-					},
-					mainHandWeaponAV : 0,
-					mainHandWeaponDV : 0,
-					offHandWeaponAV : 0,
-					offHandWeaponDV : 0
-				},
-				action : [
-					{
-						type: '',
-						attackType: '',
-						bonusAVUsed: 0,
-						location: {
-							name: '',
-							vigorCost: 0
-						},
-						specials: {
-							name: '',
-							vigorCost: 0
-						},
-						effects: {
-							name: '',
-							vigorCost: 0
-						},
-						roll: 0,
-						finalAV: 0,
-						vigor: 0,
-						luck: 0,
-						sublocation: '',
-						damageDone: 0,
-						successfulEffects: ''
-					},
-					{
-						type: '',
-						attackType: '',
-						bonusAVUsed: 0,
-						location: {
-							name: '',
-							vigorCost: 0
-						},
-						specials: {
-							name: '',
-							vigorCost: 0
-						},
-						effects: {
-							name: '',
-							vigorCost: 0
-						},
-						roll: 0,
-						finalAV: 0,
-						vigor: 0,
-						luck: 0,
-						sublocation: '',
-						damageDone: 0,
-						successfulEffects: ''
-					},
-					{
-						type: '',
-						attackType: '',
-						bonusAVUsed: 0,
-						location: {
-							name: '',
-							vigorCost: 0
-						},
-						specials: {
-							name: '',
-							vigorCost: 0
-						},
-						effects: {
-							name: '',
-							vigorCost: 0
-						},
-						roll: 0,
-						finalAV: 0,
-						vigor: 0,
-						luck: 0,
-						sublocation: '',
-						damageDone: 0,
-						successfulEffects: ''
-					},
-					{
-						type: '',
-						attackType: '',
-						bonusAVUsed: 0,
-						location: {
-							name: '',
-							vigorCost: 0
-						},
-						specials: {
-							name: '',
-							vigorCost: 0
-						},
-						effects: {
-							name: '',
-							vigorCost: 0
-						},
-						roll: 0,
-						finalAV: 0,
-						vigor: 0,
-						luck: 0,
-						sublocation: '',
-						damageDone: 0,
-						successfulEffects: ''
-					}
-				]
-			};
-		};
-
-		$scope.endTurn = function() {
-			turnsSrv.history.push($scope.thisTurn);
-			$scope.resetTurn();
-		};
-
-		$scope.undoTurn = function() {
-			turnsSrv.history.splice(turnsSrv.history.length - 1, 1);
-		}
+		$scope.setMHActiveSkill(character.currentMainHandSkill);
 
 	}]);
--- app/modules/common/services/turns-srv.js
+++ app/modules/common/services/turns-srv.js
@@ -1,13 +1,145 @@
 'use strict';
 
-angular.module('myApp.services').service('turnsSrv', function() {
+angular.module('myApp.services').service('turnsSrv', ['character', function(character) {
 	var turns = {};
 
-	/**
-	 * This is history for all turns
-	 * @type {Array}
-	 */
-	turns.history = [];
+	turns.combat = [];
+
+	turns.endTurn = function() {
+		var tempTurn = {};
+
+		angular.copy(turns.thisTurn, tempTurn);
+
+		tempTurn.index = turns.combat.length;
+		turns.combat.push(tempTurn);
+	};
+
+	turns.spendLuck = function(n) {
+		if(character.luck >= n) {
+			character.luck -= n;
+			return character.luck;
+		} else {
+			return false;
+		}
+	}
+
+	turns.thisTurn = {
+		index : 0,
+		initiative : 0,
+		bonusAV : 0,
+		equipment : {
+			mainHandWeaponSkill : {
+				name: '',
+				level: 0
+			},
+			offHandWeaponSkill : {
+				name: '',
+				level: 0
+			},
+			mainHandWeaponAV : 0,
+			mainHandWeaponDV : 0,
+			offHandWeaponAV : 0,
+			offHandWeaponDV : 0
+		},
+		action : [
+			{
+				type: '',
+				attackType: '',
+				bonusAVUsed: 0,
+				location: {
+					name: '',
+					vigorCost: 0
+				},
+				specials: {
+					name: '',
+					vigorCost: 0
+				},
+				effects: {
+					name: '',
+					vigorCost: 0
+				},
+				roll: 0,
+				finalAV: 0,
+				vigor: 0,
+				luck: 0,
+				sublocation: '',
+				damageDone: 0,
+				successfulEffects: ''
+			},
+			{
+				type: '',
+				attackType: '',
+				bonusAVUsed: 0,
+				location: {
+					name: '',
+					vigorCost: 0
+				},
+				specials: {
+					name: '',
+					vigorCost: 0
+				},
+				effects: {
+					name: '',
+					vigorCost: 0
+				},
+				roll: 0,
+				finalAV: 0,
+				vigor: 0,
+				luck: 0,
+				sublocation: '',
+				damageDone: 0,
+				successfulEffects: ''
+			},
+			{
+				type: '',
+				attackType: '',
+				bonusAVUsed: 0,
+				location: {
+					name: '',
+					vigorCost: 0
+				},
+				specials: {
+					name: '',
+					vigorCost: 0
+				},
+				effects: {
+					name: '',
+					vigorCost: 0
+				},
+				roll: 0,
+				finalAV: 0,
+				vigor: 0,
+				luck: 0,
+				sublocation: '',
+				damageDone: 0,
+				successfulEffects: ''
+			},
+			{
+				type: '',
+				attackType: '',
+				bonusAVUsed: 0,
+				location: {
+					name: '',
+					vigorCost: 0
+				},
+				specials: {
+					name: '',
+					vigorCost: 0
+				},
+				effects: {
+					name: '',
+					vigorCost: 0
+				},
+				roll: 0,
+				finalAV: 0,
+				vigor: 0,
+				luck: 0,
+				sublocation: '',
+				damageDone: 0,
+				successfulEffects: ''
+			}
+		]
+	};
 
 	return turns;
-});
+}]);
