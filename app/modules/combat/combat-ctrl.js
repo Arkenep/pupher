be3bcfac7a6f19c897b046973b12520d7d685e33
Nim <nim@nim.lt>
2016-04-10 15:31:23 +0300

    removed many scope objects that was not essential. replaced with character object

--- app/modules/combat/combat-ctrl.js
+++ app/modules/combat/combat-ctrl.js
@@ -13,53 +13,30 @@ angular.module('myApp.combat', ['ngRoute'])
 			}
 		};
 
-		$scope.character = character; //cia susitrumpina ir reikia pareiti per visus sh kuriuos levai apsirasiau.
-
-		//console.log(character);
+		$scope.character = character;
+		
 		$scope.autofail = 6;
-		//$scope.luck = character.luck;
-
-		$scope.currentMainHandSkill = character.currentMainHandSkill;
-		$scope.currentOffHandSkill = character.currentOffHandSkill;
-
-		$scope.characterMHAV = character.characterMHAV;
-		$scope.characterMHDV = character.characterMHDV;
-		$scope.characterOHAV = character.characterOHAV;
-		$scope.characterOHDV = character.characterOHDV;
-
-
-
-		$scope.activeSkills = character.combatSkills;
-		$scope.weapons = character.weapons;
-		$scope.mainHandWeapon = character.mainHandWeapon;
-		$scope.offHandWeapon = character.offHandWeapon;
-
+		
 		$scope.setMainHandWeapon = function(weapon) {
 			character.mainHandWeapon = weapon;
-			$scope.mainHandWeapon = weapon;
 		};
 
 		$scope.setOffHandWeapon = function(weapon) {
 			character.offHandWeapon = weapon;
-			$scope.offHandWeapon = weapon;
 		};
 
 		$scope.setMHActiveSkill = function(skill) {
-			$scope.currentMainHandSkill = skill;
 			character.currentMainHandSkill = skill;
-			$scope.characterMHAV = character.combatSkills[skill] + character.attributes.REF + $scope.mainHandWeapon.attackValueBonus;
-			$scope.characterMHDV = character.combatSkills[skill] + character.attributes.REF + $scope.mainHandWeapon.defenceValueBonus;
-
+			character.characterMHAV = character.combatSkills[skill] + character.attributes.REF + character.mainHandWeapon.attackValueBonus;
+			character.characterMHDV = character.combatSkills[skill] + character.attributes.REF + character.mainHandWeapon.defenceValueBonus;
 		};
 
 		$scope.setOHActiveSkill = function(skill) {
-			$scope.currentOffHandSkill = skill;
 			character.currentOffHandSkill = skill;
-			$scope.characterOHAV = character.combatSkills[skill] + character.attributes.REF + $scope.offHandWeapon.attackValueBonus;
-			$scope.characterOHDV = character.combatSkills[skill] + character.attributes.REF + $scope.offHandWeapon.defenceValueBonus;
-
+			character.characterOHAV = character.combatSkills[skill] + character.attributes.REF + character.offHandWeapon.attackValueBonus;
+			character.characterOHDV = character.combatSkills[skill] + character.attributes.REF + character.offHandWeapon.defenceValueBonus;
 		};
 
 		$scope.setMHActiveSkill(character.currentMainHandSkill);
-
+		$scope.setOHActiveSkill(character.currentOffHandSkill);
 	}]);
